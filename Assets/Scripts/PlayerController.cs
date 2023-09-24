0fc2204c4ab4657dd437c0fcfa7f6f41e1ec1671
Jaden243 <144974650+Jaden243@users.noreply.github.com>
2023-09-24 15:05:18 -0400

    Scene_1_Text_Fix Camera
    Applied text and fixed Camera position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
-    private Rigidbody rb;
+    public Text countText;
+    public Text winText;
 
-    private void Start()
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
     void FixedUpdate ()
     {
@@ -26,6 +34,17 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    
+    void SetCountText()
+    {
+       countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
         }
     }
 }
